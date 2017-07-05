a2c59c0489dd00464bfaed9e0381934977ae06ac
Your Name <you@example.com>
2017-07-05 11:57:18 -0400

    continued

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -36,7 +36,7 @@ namespace Week2
             //Console.WriteLine(childAge[0]);
             //Console.WriteLine(childAge[1]);
             //Console.WriteLine(childAge[2]);
-            
+
             ////Array of GPA on a 4.0 scale of 8 students - you don't have to print
             //double[] gpa = { 3.2, 3.0, 2.9, 4.0, 3.9, 2.0, 3.7, 3.1 };
 
@@ -46,6 +46,22 @@ namespace Week2
             //for (int i = 0; i < firstLetter.Length; i++)
             //    Console.Write(firstLetter[i]);
 
+            //string[] studentsInClass = new string[21];
+            //studentsInClass[0] = "Jordan";
+
+            //Console.WriteLine("Please enter the name of the next student.");
+            //studentsInClass[1] = Console.ReadLine();
+           
+            ////Ask user for input
+            //Console.WriteLine("Please enter the age of the person in the first seat.");
+            ////Create a new int array with 21 spots and call it ageOfStudentsInClass
+            //int[] ageOfStudentsInClass = new int[21];
+            ////Assign the value of the int at the 0 index of ageOfStudentsInClass to ...
+            ////... the user's input converted into an int
+            //ageOfStudentsInClass[0] = int.Parse(Console.ReadLine());
+            ////Create a new string variable and set it equal to the int at the index of 0 ...
+            ////and convert it into a string
+            //string firstIndex = ageOfStudentsInClass[0].ToString();
         }
     }
 }
