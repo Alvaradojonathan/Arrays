edef3a5de2b797205aff85f7f577ec423b2d67df
Your Name <you@example.com>
2017-07-05 11:08:38 -0400

    Arrays Practice

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -10,6 +10,42 @@ namespace Week2
     {
         static void Main(string[] args)
         {
+            ////Declaring array without initializing it.
+            //int[] myFirstArray;
+
+            ////Declare and initialize a second array
+            //string[] mySecondArray = { "firstElement", "secondElement", "thirdElement", "fourthElement" };
+
+            ////Declare and inititalize a char array with the letters of your last name
+            //char[] lastName = { 'a', 'l', 'v', 'a', 'r', 'a', 'd', 'o' };
+
+            //Console.WriteLine(lastName[0]);
+            //Console.WriteLine(lastName[3]);
+            //Console.WriteLine(lastName[1]);
+
+            //Create the following arrays and print using the correct index numbers:
+            //Array of top ten vacation spots
+            //Print the first and last vacation spot
+            //string[] vacationSpots = { "Sydney", "Orlando", "Puerto Rico", "Cuba", "California", "Canada", "DC", "Italy", "Tokyo", "Mexico" };
+            //Console.WriteLine(vacationSpots[0]);
+            //Console.WriteLine(vacationSpots[9]);
+
+            ////Array of ages of 3 children you know
+            ////Print each child's age
+            //int[] childAge = { 8, 7, 7 };
+            //Console.WriteLine(childAge[0]);
+            //Console.WriteLine(childAge[1]);
+            //Console.WriteLine(childAge[2]);
+            
+            ////Array of GPA on a 4.0 scale of 8 students - you don't have to print
+            //double[] gpa = { 3.2, 3.0, 2.9, 4.0, 3.9, 2.0, 3.7, 3.1 };
+
+            ////Array of the first letter of four of your classmate's names who are sitting near you
+            ////Print all four on the same line
+            //char[] firstLetter = { 'J', 'D', 'M', 'B' };
+            //for (int i = 0; i < firstLetter.Length; i++)
+            //    Console.Write(firstLetter[i]);
+
         }
     }
 }
